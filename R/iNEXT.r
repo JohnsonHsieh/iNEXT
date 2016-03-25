1f598c6117f52231b9a94a22308cb1249bd13e6b
Johnson <euler96@gmail.com>
2016-03-25 19:33:33 +0800

    add bird (data.frame) example

--- R/iNEXT.r
+++ R/iNEXT.r
@@ -536,15 +536,23 @@ iNEXT.Sam <- function(Spec, t=NULL, q=0, endpoint=2*max(Spec), knots=40, se=TRUE
 #' \code{$iNextEst} for showing diversity estimates for rarefied and extrapolated samples along with related statistics;
 #' and \code{$AsyEst} for showing asymptotic diversity estimates along with related statistics.  
 #' @examples
+#' ## example for abundance based data (list of vector)
 #' data(spider)
 #' out1 <- iNEXT(spider, q=0, datatype="abundance")
 #' out1$DataInfo # showing basic data information.
 #' out1$AsyEst # showing asymptotic diversity estimates.
 #' out1$iNextEst # showing diversity estimates with rarefied and extrapolated.
+#' 
+#' ## example for abundance based data (data.frame)
+#' data(bird)
+#' out2 <- iNEXT(bird, q=0, datatype="abundance")
+#' ggiNEXT(out2)
+#' 
+#' ## example for incidence frequencies based data (list of data.frame)
 #' data(ant)
 #' t <- round(seq(10, 500, length.out=20))
-#' out2 <- iNEXT(ant$h500m, q=1, datatype="incidence_freq", size=t, se=FALSE)
-#' out2$iNextEst
+#' out3 <- iNEXT(ant$h500m, q=1, datatype="incidence_freq", size=t, se=FALSE)
+#' out3$iNextEst
 #' 
 #' @export
 #' 
