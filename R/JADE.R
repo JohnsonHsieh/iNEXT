c64710646ccb470be2382580026940fa3d125109
AnneChao <ljchao2012@gmail.com>
2019-01-17 14:46:05 +0800

    Update JADE.R
    add DetAbu function

--- R/JADE.R
+++ R/JADE.R
@@ -58,3 +58,27 @@ EstComDis <- function(x){
   phat
 }
 
+#====20190117 added====
+DetAbu <- function(x, zero=FALSE){
+  x <- unlist(x)
+  n <- sum(x)  
+  f1 <- sum(x==1)
+  f2 <- sum(x==2)
+  f3 <- sum(x==3)
+  if(f2==0){
+    f1 <- max(f1 - 1, 0)
+    f2 <- 1
+  }
+  A1 <- f1 / n * ((n-1)*f1 / ((n-1)*f1 + 2*max(f2,1)))
+  A2 <- f2 / choose(n, 2) * ((n-2)*f2 / ((n-2)*f2 + 3*max(f3,1)))^2
+  if(zero==FALSE) x <- x[x>0]
+  q.solve <- function(q){
+    e <- A1 / sum(x/n*exp(-q*x))
+    out <- sum((x/n * (1 - e * exp(-q*x)))^2) - sum(choose(x,2)/choose(n,2)) + A2
+    abs(out)
+  }
+  q <- tryCatch(optimize(q.solve, c(0,1))$min, error = function(e) {1})
+  e <- A1 / sum(x/n*exp(-q*x))
+  o <- x/n * (1 - e * exp(-q*x))
+  o
+}
